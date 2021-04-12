7276c77df7a96b6ee78ef1b8b5a630bea993af91
rohith <rohiths789@gmail.com>
2021-04-12 10:02:22 +0530

    my karnataka bank

---
+++ footer.php
@@ -0,0 +1,42 @@
+<footer id="footer" class="bg-dark text-white mt-5 py-5">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-3 col-12">
+                <h4 class="font-rubik font-size-20">Contact us</h4>
+                <p class="font-size-14 font-rale text-white-50">KR.Puram near tech park bangalore 49 (+91) 991610044<br> Email: <a href="#" class="color-second">Karnatakabank@gmail.com</a></p>
+              </div>
+              <div class="col-lg-4 col-12">
+                <h4 class="font-rubik font-size-20">For more information</h4>
+                <form class="form-row">
+                  <div class="col">
+                    <input type="text" class="form-control" placeholder="Email *">
+                  </div>
+                  <div class="col">
+                    <button type="submit" class="btn btn-primary mb-2">Subscribe</button>
+                  </div>
+                </form>
+              </div>
+              <div class="col-lg-2 col-12">
+                <h4 class="font-rubik font-size-20">Information</h4>
+                <div class="d-flex flex-column flex-wrap">
+                  <a href="#" class="font-rale font-size-14 text-white-50 pb-1">About Us</a>
+                  <a href="#" class="font-rale font-size-14 text-white-50 pb-1">Find your nearest bank</a>
+                  <a href="#" class="font-rale font-size-14 text-white-50 pb-1">Privacy Policy</a>
+                  <a href="#" class="font-rale font-size-14 text-white-50 pb-1">Terms & Conditions</a>
+                </div>
+              </div>
+              <div class="col-lg-2 col-12">
+                <h4 class="font-rubik font-size-20">Account</h4>
+                <div class="d-flex flex-column flex-wrap">
+                  <a href="#" class="font-rale font-size-14 text-white-50 pb-1">My Account</a>
+                  <a href="#" class="font-rale font-size-14 text-white-50 pb-1">Users</a>
+                  <a href="#" class="font-rale font-size-14 text-white-50 pb-1">Transaction</a>
+                  <a href="#" class="font-rale font-size-14 text-white-50 pb-1">Netbanking</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+        <div class="copyright text-center bg-dark text-white py-2">
+          <p class="font-rale font-size-14">&copy; Copyrights 2020. Desing By <a href="#" class="color-second">Rohith</a></p>
+        </div>
---
+++ navbar.php
@@ -0,0 +1,25 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light" style="background-color :#FF0000 ;">
+      <a class="navbar-brand" href="index.php" style="color : #ffffff;"><b>Karnataka bank</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #ffffff;"><b>Dashboard</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #ffffff;"><b>User Registration</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #ffffff;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #ffffff;"><b>Transaction History</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" style="color : #ffffff;"><b>About Us</b></a>
+              </li>
+          </div>
+       </nav>
